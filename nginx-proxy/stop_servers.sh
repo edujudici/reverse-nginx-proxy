292f2bc6d1b3eaf0f27ccd156f9d861f52867cda
Eduardo Judici <eduardo.judici@ciandt.com>
2025-04-22 15:37:03 -0300

    fix: find symlinks

--- nginx-proxy/stop_servers.sh
+++ nginx-proxy/stop_servers.sh
@@ -5,11 +5,14 @@ docker-compose down
 filelist=""
 
 for d in ../sites/*/ ; do
-    current_dir=$(find "$d" -maxdepth 1 -type d -name '*-current')
+    # Encontra o link simbólico com sufixo -current dentro de cada domínio
+    current_dir=$(find "$d" -maxdepth 1 -lname '*-current' -o -name '*-current' -type l)
+    echo Including $current_dir
+
     if [ -n "$current_dir" ]; then
         compose_file="${current_dir}/docker-compose-production.yml"
-
         echo Including $compose_file
+
         n="-f ${compose_file}"
         filelist="${filelist}$n "
     fi
