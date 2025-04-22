c13015a050575eacd9ac33184f522aa7982ba3cf
Eduardo Judici <eduardo.judici@ciandt.com>
2025-04-22 15:46:10 -0300

    fix: set env file

--- nginx-proxy/stop_servers.sh
+++ nginx-proxy/stop_servers.sh
@@ -3,20 +3,33 @@ logger Stopping docker webserver cluster
 docker-compose down
 
 filelist=""
+envfiles=()
 
 for d in ../sites/*/ ; do
-    # Encontra o link simbólico com sufixo -current dentro de cada domínio
-    current_dir=$(find "$d" -maxdepth 1 -lname '*-current' -o -name '*-current' -type l)
-    echo Including $current_dir
+    current_dir=$(find "$d" -maxdepth 1 -type l -name '*-current')
 
     if [ -n "$current_dir" ]; then
         compose_file="${current_dir}/docker-compose-production.yml"
-        echo Including $compose_file
+        env_file="${current_dir}/src/.env"
 
-        n="-f ${compose_file}"
-        filelist="${filelist}$n "
+        if [ -f "$compose_file" ]; then
+            echo "Including $compose_file"
+            filelist="${filelist}-f $compose_file "
+        fi
+
+        if [ -f "$env_file" ]; then
+            envfiles+=("--env-file $env_file")
+        fi
     fi
 done
 
-docker compose ${filelist} down
+# Junta todos os --env-file em uma única string
+env_args="${envfiles[*]}"
+
+# Exibe os arquivos incluídos (opcional)
+echo "Docker Compose Filelist: $filelist"
+echo "Env files: $env_args"
+
+# Executa o docker compose down com os arquivos e os respectivos env-files
+docker compose $filelist $env_args down
 
